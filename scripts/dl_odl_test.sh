6319e46af0432288296cc79af0aabc4fa0e59a6a
Daniel Farrell <dfarrell@redhat.com>
2015-02-18 15:35:19 -0500

    Major updates to ODL dl test script
    * Add ability to loop for multiple tests
    * Accept param for number of tests to run, default to 1
    * Bugfix in var names (dl_dr instead of dl_dir)
    * More verbose test output
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- scripts/dl_odl_test.sh
+++ scripts/dl_odl_test.sh
@@ -8,28 +8,42 @@ dl_dir="."  # This will normally be a good default for non-root runs
 dl_name="opendaylight-0.2.2.tar.gz"
 odl_sha256="4461c0be129b84c98f4e02a8e3682d82bb14ca497ce91c87c5385cae6ddad3c8"
 
-# Remove old ODL install if present
-if [ -f $dl_dr/$dl_name ]; then
-    echo "Removing old ODL dl at $dl_dr/$dl_name"
-    rm -rf $dl_dr/$dl_name
-fi
-
-# Download ODL using same command as Archive::Download
-# NB: This command is from the trace in #12.
-#   * The `-s` (silent) flag was removed to allow debugging feedback
-#   * The output path was made into vars for shared access
-#   * The output dir may be configured to use CWD vs `/usr/src` (avoids root)
-#   * Some extra spaces were removed
-curl -L -o $dl_dir/$dl_name https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.2.2-Helium-SR2/distribution-karaf-0.2.2-Helium-SR2.tar.gz
-
-# Validate that download was successful
-dl_sha256=`sha256sum $dl_dir/$dl_name | awk '{print $1}'`
-echo "SHA256 hash of dl: $dl_sha256"
-
-if [ "$dl_sha256" == "$odl_sha256" ]; then
-  echo "Hash verified"
+# Accept a single (optional) param for number of runs
+if [ "$#" -eq 1 ]; then
+    num_runs=$1
 else
-  echo "Error: Hash didn't match expected ODL SHA256"
-  echo "Expected ODL SHA256: $odl_sha256"
-  exit 1
+    # Default to 1 run
+    num_runs=1
 fi
+
+# Download and validate ODL num_runs times
+for ((run=0;run<$num_runs;run++)); do
+    echo "# Starting run $run"
+
+    # Remove old ODL install if present
+    if [ -f $dl_dir/$dl_name ]; then
+        echo "Removing old ODL dl at $dl_dir/$dl_name"
+        rm -rf $dl_dir/$dl_name
+    fi
+
+    # Download ODL using same command as Archive::Download
+    # NB: This command is from the trace in #12.
+    #   * The output path was made into vars for shared access
+    #   * The output dir may be configured to use CWD vs `/usr/src` (avoids root)
+    #   * Some extra spaces were removed
+    curl -s -L -o $dl_dir/$dl_name https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.2.2-Helium-SR2/distribution-karaf-0.2.2-Helium-SR2.tar.gz
+
+    # Validate that download was successful
+    dl_sha256=`sha256sum $dl_dir/$dl_name | awk '{print $1}'`
+    echo "SHA256 hash of dl: $dl_sha256"
+
+    if [ "$dl_sha256" == "$odl_sha256" ]; then
+        echo "Hash verified"
+    else
+        echo "Error: Hash didn't match expected ODL SHA256"
+        echo "Expected ODL SHA256: $odl_sha256"
+        exit 1
+    fi
+
+    echo "# Ending run $run"
+done
