2a6aa70d95f4f04d589caeea39b75d9e3633d01f
Daniel Farrell <dfarrell@redhat.com>
2015-02-18 13:34:52 -0500

    Stub of script to test dl of ODL tarball
    * Relevant to issue #12
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ scripts/dl_odl_test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+# Script to repeatedly test downloading OpenDaylight tarball
+# Created to help debug issue #12. See:
+# https://github.com/dfarrell07/vagrant-opendaylight/issues/12
+
+# TODO: Remove old ODL install if present
+
+# TODO: Download ODL using same command as Archive::Download
+
+# TODO: Validate that download was successful
